98d4617e8d0d56a958417eef8a08fb3418cf2099
Anton Artamasav <antonart9@gmail.com>
2024-11-27 16:13:25 -0500

    Removed large files

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import folium
 import pandas as pd
 import numpy as np
 import webbrowser
+import duckdb
+import gdown 
 
-nfip_loss_r = pd.read_csv("multipleLossProperties.csv", index_col=0)
+url  ="https://drive.google.com/file/d/1_2aV0idVYIhufdc-V9A7tZ_xhrqy4OcB/view?usp=drive_link"
+output = "data/multipleLossProperties2.csv"
+gdown.download(url, output, fuzzy=True)
+
+
+nfip_loss_r = pd.read_csv("data/multipleLossProperties2.csv", index_col=0) 
 
 nfip_loss_r['asOfDate - Effective date on the File'] = pd.to_datetime(nfip_loss_r['asOfDate - Effective date on the File'])
 nfip_loss_r["asOfDate - Effective date on the File"] = nfip_loss_r['asOfDate - Effective date on the File'].dt.date
@@ -24,17 +31,17 @@ print(nfip_loss.head())
 
 nfip_loss.fillna(0)
 
-nfip_loss.to_csv('nfip_loss_cld.csv', index=False)
+nfip_loss.to_csv('data/nfip_loss_cld.csv', index=False)
 
 
-m = folium.Map(location=[48, -102], zoom_start=5)
+m = folium.Map(location=[44, -102], zoom_start=5)
 
-for index, row in nfip_loss.iterrows():
-    if pd.notnull(row['latitude']) and pd.notnull(row['longitude']):
-        folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['reportedCity']
-        ).add_to(m)
+#for index, row in nfip_loss.iterrows():
+    #if pd.notnull(row['latitude']) and pd.notnull(row['longitude']):
+       # folium.Marker(
+        #location=[row['latitude'], row['longitude']],
+        #popup=row['reportedCity']
+        #).add_to(m)
 
 m.save("m.html")
 webbrowser.open("m.html")
