c3ddf99999c7d05f6073ba65c9dad032176fd2dc
Anton Artamasav <antonart9@gmail.com>
2024-12-04 18:16:41 -0500

    Added a list of coordinates organized by region ID. Pulled that list into folium and generated some polygons on the map. Need to clean data.

--- main.py
+++ main.py
@@ -27,14 +27,11 @@ nfip_loss_r['mostRecentDateofLoss'] = pd.to_datetime(nfip_loss_r['mostRecentDate
 nfip_loss_r["mostRecentDateofLoss"] = nfip_loss_r['mostRecentDateofLoss'].dt.date
 
 nfip_loss = nfip_loss_r.drop(columns=["state", "id", "censusBlockGroup"])
-print(nfip_loss.head())
+#print(nfip_loss.head())
 
 nfip_loss.fillna(0)
 
 nfip_loss.to_csv('data/nfip_loss_cld.csv', index=False)
-
-
-m = folium.Map(location=[44, -102], zoom_start=5)
 
 #for index, row in nfip_loss.iterrows():
     #if pd.notnull(row['latitude']) and pd.notnull(row['longitude']):
@@ -43,9 +40,31 @@ m = folium.Map(location=[44, -102], zoom_start=5)
         #popup=row['reportedCity']
         #).add_to(m)
 
-m.save("m.html")
-webbrowser.open("m.html")
+#m.save("m.html")
+#webbrowser.open("m.html")
 
+#flds_by_year = duckdb.sql("SELECT YEAR(mostRecentDateofLoss) as year, COUNT(*) AS total_floods FROM nfip_loss GROUP BY YEAR(mostRecentDateofLoss) ORDER BY year; ")
+
+#flds_by_zip = duckdb.sql("SELECT communityIdNumber, stateAbbreviation, COUNT(zipCode) as ZIPcnt, FROM nfip_loss GROUP BY stateAbbreviation, communityIdNumber ORDER BY communityIdNumber;")
+
+group_coord_querry = """SELECT list([latitude, longitude]) AS coordinate_groups 
+                        FROM nfip_loss 
+                        WHERE latitude 
+                        IS NOT NULL AND longitude 
+                        IS NOT NULL GROUP BY communityIdNumber;"""
+
+grouped_com_coords = duckdb.execute(group_coord_querry).fetchall()
+
+map = folium.Map(location=[37.7749, -122.4194], zoom_start=5)
+
+for row in grouped_com_coords:
+        coords = row[0]
+        folium.Polygon(locations=coords, color="red", weight=2.5, fill=True, fill_opacity=0.5).add_to(map)
+
+map.save("map.html")
+webbrowser.open("map.html")
+
+print(grouped_com_coords)
 
 
 
